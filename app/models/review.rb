cebf25288ede6ff65637e485dc3e6949ff037fd0
Marc della Faille <marcdellafaille@gmail.com>
2021-05-26 16:35:28 +0200

    Yelp MVP

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 0
+  }
+  validates :content, presence: true
+end
